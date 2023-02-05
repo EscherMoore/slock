f6b7effaa19a2e7f9cb99198a767095cc402ffbc
EscherMoore <EscherMoore@users.noreply.github.com>
2023-02-05 00:09:58 -0500

    change from default colors to gruvbox colors

--- config.def.h
+++ config.def.h
@@ -1,11 +1,11 @@
 /* user and group to drop privileges to */
-static const char *user  = "nobody";
-static const char *group = "nogroup";
+static const char *user  = "escher";
+static const char *group = "escher";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+	[INPUT] =  "#83a598",   /* during input */
+	[FAILED] = "#cc241d",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
